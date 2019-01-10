79de72575a9a7c66c5cbca888a3a2c8666ae2a08
robert <lloudb@gmail.com>
2019-01-10 16:23:45 +0300

    commit

--- cache/twig/9c/9c83b910f50954193c13861f0798b60431a1d14e0c30e69ff14fd216ca71365e.php
+++ cache/twig/9c/9c83b910f50954193c13861f0798b60431a1d14e0c30e69ff14fd216ca71365e.php
@@ -15,14 +15,13 @@ class __TwigTemplate_45783e80b680fd0a1de9d4c4142b22b54a620cf1e3153899c9dd6df9746
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        // line 18
+        // line 26
         echo "
-<ul class=\"navbar-nav text-uppercase g-pos-rel g-font-weight-600 ml-auto\">
-    ";
-        // line 20
+
+";
+        // line 28
         echo $this->getAttribute($this, "loop", array(0 => ($context["pages"] ?? null)), "method");
         echo "
-</ul>
 ";
     }
 
@@ -41,30 +40,60 @@ class __TwigTemplate_45783e80b680fd0a1de9d4c4142b22b54a620cf1e3153899c9dd6df9746
             // line 2
             echo "  ";
             $context['_parent'] = $context;
-            $context['_seq'] = twig_ensure_traversable($this->getAttribute($this->getAttribute(($context["page"] ?? null), "children", array()), "visible", array()));
+            $context['_seq'] = twig_ensure_traversable($this->getAttribute(($context["page"] ?? null), "children", array()));
             foreach ($context['_seq'] as $context["_key"] => $context["p"]) {
                 // line 3
                 echo "    ";
-                if (($this->getAttribute($this->getAttribute($this->getAttribute($context["p"], "children", array()), "visible", array()), "count", array()) > 0)) {
+                if (($this->getAttribute($context["p"], "visible", array()) && ($this->getAttribute($this->getAttribute($context["p"], "children", array()), "count", array()) > 0))) {
                     // line 4
-                    echo "    <li class=\"hs-has-mega-menu nav-item  g-mx-10--lg g-mx-15--xl\" data-animation-in=\"fadeIn\" data-animation-out=\"fadeOut\" data-max-width=\"60%\" data-position=\"left\">
-      <a id=\"nav-link--features\" class=\"nav-link g-py-7 g-px-0\" href=\"#!\" aria-haspopup=\"true\" aria-expanded=\"false\" aria-controls=\"nav-submenu--features\">
-        ";
-                    // line 6
+                    echo "        <li class=\"nav-item hs-has-sub-menu  g-mx-10--lg g-mx-15--xl\" data-animation-in=\"fadeIn\" data-animation-out=\"fadeOut\">
+            <a id=\"nav-link--features\" class=\"nav-link g-py-7 g-px-0\" href=\"#!\" aria-haspopup=\"true\" aria-expanded=\"false\" aria-controls=\"nav-submenu--features\">";
+                    // line 5
                     echo $this->getAttribute($context["p"], "menu", array());
-                    echo "
-      <i class=\"hs-icon hs-icon-arrow-bottom g-font-size-11 g-ml-7\"></i>
-      </a>
-      <ul class=\"hs-sub-menu list-unstyled u-shadow-v11 g-brd-top g-brd-primary g-brd-top-2 g-min-width-220 g-mt-18 g-mt-8--lg--scrolling\" id=\"nav-submenu--features\" aria-labelledby=\"nav-link--features\">
-        <li class=\"dropdown-item \">
-          <a class=\"nav-link\" href=\"../../unify-main/shortcodes/headers/index.html\">Headers</a>
+                    echo "</a>
+
+            <ul class=\"hs-sub-menu list-unstyled u-shadow-v11 g-brd-top g-brd-primary g-brd-top-2 g-min-width-220 g-mt-18 g-mt-8--lg--scrolling\" id=\"nav-submenu--features\" aria-labelledby=\"nav-link--features\">
+            ";
+                    // line 8
+                    $context['_parent'] = $context;
+                    $context['_seq'] = twig_ensure_traversable($this->getAttribute($context["p"], "children", array()));
+                    foreach ($context['_seq'] as $context["_key"] => $context["pc"]) {
+                        // line 9
+                        echo "                    <li class=\"dropdown-item \">
+                        <a class=\"nav-link\" href=\"";
+                        // line 10
+                        echo $this->getAttribute($context["pc"], "url", array());
+                        echo "\">";
+                        echo $this->getAttribute($context["pc"], "menu", array());
+                        echo "</a>
+                    </li>
+            ";
+                    }
+                    $_parent = $context['_parent'];
+                    unset($context['_seq'], $context['_iterated'], $context['_key'], $context['pc'], $context['_parent'], $context['loop']);
+                    $context = array_intersect_key($context, $_parent) + $_parent;
+                    // line 13
+                    echo "            </ul>
+
+
         </li>
-      </ul>
-    </li>
+
+    ";
+                } else {
+                    // line 19
+                    echo "        <li class=\"nav-item  g-mx-10--lg g-mx-15--xl\">
+            <a href=\"";
+                    // line 20
+                    echo $this->getAttribute($context["p"], "url", array());
+                    echo "\" class=\"nav-link g-py-7 g-px-0\">";
+                    echo $this->getAttribute($context["p"], "menu", array());
+                    echo "</a>
+        </li>
     ";
                 }
-                // line 16
-                echo "  ";
+                // line 23
+                echo "
+  ";
             }
             $_parent = $context['_parent'];
             unset($context['_seq'], $context['_iterated'], $context['_key'], $context['p'], $context['_parent'], $context['loop']);
@@ -94,7 +123,7 @@ class __TwigTemplate_45783e80b680fd0a1de9d4c4142b22b54a620cf1e3153899c9dd6df9746
 
     public function getDebugInfo()
     {
-        return array (  67 => 16,  54 => 6,  50 => 4,  47 => 3,  42 => 2,  30 => 1,  23 => 20,  19 => 18,);
+        return array (  95 => 23,  87 => 20,  84 => 19,  76 => 13,  65 => 10,  62 => 9,  58 => 8,  52 => 5,  49 => 4,  46 => 3,  41 => 2,  29 => 1,  23 => 28,  19 => 26,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -108,26 +137,33 @@ class __TwigTemplate_45783e80b680fd0a1de9d4c4142b22b54a620cf1e3153899c9dd6df9746
     public function getSourceContext()
     {
         return new Twig_Source("{% macro loop(page) %}
-  {% for p in page.children.visible %}
-    {% if p.children.visible.count > 0 %}
-    <li class=\"hs-has-mega-menu nav-item  g-mx-10--lg g-mx-15--xl\" data-animation-in=\"fadeIn\" data-animation-out=\"fadeOut\" data-max-width=\"60%\" data-position=\"left\">
-      <a id=\"nav-link--features\" class=\"nav-link g-py-7 g-px-0\" href=\"#!\" aria-haspopup=\"true\" aria-expanded=\"false\" aria-controls=\"nav-submenu--features\">
-        {{ p.menu }}
-      <i class=\"hs-icon hs-icon-arrow-bottom g-font-size-11 g-ml-7\"></i>
-      </a>
-      <ul class=\"hs-sub-menu list-unstyled u-shadow-v11 g-brd-top g-brd-primary g-brd-top-2 g-min-width-220 g-mt-18 g-mt-8--lg--scrolling\" id=\"nav-submenu--features\" aria-labelledby=\"nav-link--features\">
-        <li class=\"dropdown-item \">
-          <a class=\"nav-link\" href=\"../../unify-main/shortcodes/headers/index.html\">Headers</a>
+  {% for p in page.children %}
+    {% if p.visible and p.children.count > 0 %}
+        <li class=\"nav-item hs-has-sub-menu  g-mx-10--lg g-mx-15--xl\" data-animation-in=\"fadeIn\" data-animation-out=\"fadeOut\">
+            <a id=\"nav-link--features\" class=\"nav-link g-py-7 g-px-0\" href=\"#!\" aria-haspopup=\"true\" aria-expanded=\"false\" aria-controls=\"nav-submenu--features\">{{ p.menu }}</a>
+
+            <ul class=\"hs-sub-menu list-unstyled u-shadow-v11 g-brd-top g-brd-primary g-brd-top-2 g-min-width-220 g-mt-18 g-mt-8--lg--scrolling\" id=\"nav-submenu--features\" aria-labelledby=\"nav-link--features\">
+            {% for pc in p.children %}
+                    <li class=\"dropdown-item \">
+                        <a class=\"nav-link\" href=\"{{ pc.url }}\">{{ pc.menu }}</a>
+                    </li>
+            {% endfor %}
+            </ul>
+
+
         </li>
-      </ul>
-    </li>
+
+    {% else %}
+        <li class=\"nav-item  g-mx-10--lg g-mx-15--xl\">
+            <a href=\"{{ p.url }}\" class=\"nav-link g-py-7 g-px-0\">{{ p.menu }}</a>
+        </li>
     {% endif %}
+
   {% endfor %}
 {% endmacro %}
 
-<ul class=\"navbar-nav text-uppercase g-pos-rel g-font-weight-600 ml-auto\">
-    {{ _self.loop(pages) }}
-</ul>
+
+{{ _self.loop(pages) }}
 ", "partials/navigation.html.twig", "C:\\xampp\\htdocs\\grav-admin\\user\\themes\\unify\\templates\\partials\\navigation.html.twig");
     }
 }
